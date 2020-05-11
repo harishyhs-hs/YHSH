45b0f41b516cd70482df07c6808e8ade2326143e
harishyhs-hs <59998166+harishyhs-hs@users.noreply.github.com>
2020-05-11 23:33:46 +0530

    Add files via upload

---
+++ gcp_vpc_gce.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  credentials = "${file("inlaid-fire-254211-b3928da874b8.json")}"
+  project = "inlaid-fire-254211"
+  region  = "us-central1"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_subnetwork" "public-subnetwork" {
+  name          = "terraform-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+ name         = "flask-vm"
+ machine_type = "f1-micro"
+ zone         = "us-central1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
+
